94e243bd064518c4eb8da14fa9c5f35eaa233a68
Joy Brar <joybrar2001@gmail.com>
2024-06-10 14:48:06 +0530

    Field Guide done

--- frontend/src/components/Structure.jsx
+++ frontend/src/components/Structure.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 
 const Structure = () => {
   return (
-    <div>Structure</div>
+    <main className='bg-white w-full rounded-2xl shadow-lg'>
+      <div className='py-4 px-4 relative'>
+        <h1 className='text-4xl text-googleBlue-400'>
+          Your Day at Best Buy
+        </h1>
+        <div className='absolute top-4 right-4 w-32 h-32 rounded-full border-[1px]'>
+          <img src="/assets/Structure Vector.png" alt="" />
+          <div className='absolute top-[10%] left-[-15%] w-10 p-2 bg-white rounded-full border-[1px]'>
+            <img src="Google Logo.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className='bg-[#F6F7FA] w-full h-[2px]' />
+
+      <div className='px-6'>
+        <ul>
+          <FormGroup>
+            <FormControlLabel control={<Checkbox />} label="Check-in to GFA and LILO" />
+            <FormControlLabel control={<Checkbox />} label="Connect with Management" />
+            <FormControlLabel control={<Checkbox />} label="Merchandising:" />
+            <FormControlLabel control={<Checkbox />} label="Follow the weekly priorities (listed on the following slide) and training materials" />
+            <FormControlLabel control={<Checkbox />} label="Check-in to GFA and LILO" />
+            <FormControlLabel control={<Checkbox />} label="Connect with Management" />
+            <FormControlLabel control={<Checkbox />} label="Merchandising:" />
+            <FormControlLabel control={<Checkbox />} label="Follow the weekly priorities (listed on the following slide) and training materials" />
+          </FormGroup>
+        </ul>
+      </div>
+
+      <div className='w-full px-20 py-6'>
+        <img src="/assets/TSM Toolbelt.png" alt="" />
+      </div>
+    </main>
   )
 }
 
--- frontend/src/components/Weekly.jsx
+++ frontend/src/components/Weekly.jsx
@@ -2,7 +2,67 @@ import React from 'react'
 
 const Weekly = () => {
   return (
-    <div>Weekly</div>
+    <main className='bg-white w-full rounded-2xl shadow-lg'>
+      <h1 className='font-semibold text-center text-4xl my-4'>
+        Weekly Priorities
+      </h1>
+      <div className='bg-[#F6F7FA] w-full h-[2px]' />
+
+      <div className='flex flex-col gap-4 pl-4 pr-6 py-8'>
+        <div className='flex gap-4'>
+          <div className='relative w-16 h-16'>
+            <p className='absolute text-googleBlue-500 top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]'>1</p>
+            <img src="/assets/Index Vector.png" alt="" className='object-contain' />
+          </div>
+          <div className='flex-1 flex flex-col gap-4'>
+            <div>
+              <img src="/assets/Pixel 8a Launch.png" className='object-contain' alt="" />
+            </div>
+            <div className='bg-googleBlue-50 rounded-xl px-4 py-3'>
+              <h6>
+                Priority and training focus on Pixel 8a and on-self. Use the TSM execution guide when structuring visits.
+              </h6>
+            </div>
+          </div>
+        </div>
+
+
+        <div className='flex gap-4'>
+          <div className='relative w-16 h-16'>
+            <p className='absolute text-googleBlue-500 top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]'>2</p>
+            <img src="/assets/Index Vector.png" alt="" className='object-contain' />
+          </div>
+          <div className='flex-1 flex flex-col gap-4'>
+            <div>
+              <img src="/assets/Google Family Link.png" className='object-contain' alt="" />
+            </div>
+            <div className='bg-googleBlue-50 rounded-xl px-4 py-3'>
+              <h6>
+                Review the pixel 8a product information = Family Link demo with every store associate to ensure they are aware of the features to be able to drive sell out in stores.
+              </h6>
+            </div>
+          </div>
+        </div>
+
+
+        <div className='flex gap-4'>
+          <div className='relative w-16 h-16'>
+            <p className='absolute text-googleBlue-500 top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%]'>3</p>
+            <img src="/assets/Index Vector.png" alt="" className='object-contain' />
+          </div>
+          <div className='flex-1 flex flex-col gap-4'>
+            <div>
+              <img src="/assets/Google X Finals.png" className='object-contain' alt="" />
+            </div>
+            <div className='bg-googleBlue-50 rounded-xl px-4 py-3'>
+              <h6>
+                Drive sell-out at AR with Finals incentives and jerseys.
+              </h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
