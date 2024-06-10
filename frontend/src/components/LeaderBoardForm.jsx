2cd831c44304a9722f895230b14adce3050b3bac
GitMohit123 <jindalm55@gmail.com>
2024-06-11 03:48:47 +0530

    Table added

--- frontend/src/components/LeaderBoardForm.jsx
+++ frontend/src/components/LeaderBoardForm.jsx
@@ -3,17 +3,18 @@ import DatePicker from "react-multi-date-picker";
 import axios from "axios";
 import useQuery from "../utils/useQuery";
 import formatDate from "../utils/formatDate";
-
+import { FiEdit3 } from "react-icons/fi";
 
 const partners = ["AT&T", "Verizon", "T-Mobile", "Best Buy"];
 
 const PartnerButton = ({ partnerName, selectedPartner, setPartner }) => {
   return (
     <button
-      className={`px-4 py-2 border-[1px] rounded-xl ${selectedPartner === partnerName
-        ? "border-googleBlue-500 text-googleBlue-500"
-        : ""
-        }`}
+      className={`px-4 py-2 border-[1px] rounded-xl ${
+        selectedPartner === partnerName
+          ? "border-googleBlue-500 text-googleBlue-500"
+          : ""
+      }`}
       onClick={() => setPartner(partnerName)}
     >
       <div></div>
@@ -23,30 +24,57 @@ const PartnerButton = ({ partnerName, selectedPartner, setPartner }) => {
 };
 
 const LeaderBoardForm = () => {
-  const query = useQuery()
-
-  const [partner, setPartner] = useState("Verizon");
+  const query = useQuery();
+  const territory_id = query.get("territory_id");
+  const [partner, setPartner] = useState("AT&T");
   const [date, setDate] = useState("2024-05-07");
-
+  const [tableData, setTableData] = useState([]);
+  const [isEdit, setIsEdit] = useState(false);
+  const [columns, setColumns] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleDateChange = (e) => {
-    console.log(e);
-  }
+    const year = e.year;
+    const month = String(e.month.number).padStart(2, "0");
+    const day = String(e.day).padStart(2, "0");
+    const dateString = `${year}-${month}-${day}`;
+    setDate(dateString);
+  };
+  const getUniqueColumns = (data) => {
+    const columns = new Set();
+    for (const item of data) {
+      Object.keys(item).forEach((key) => columns.add(key));
+    }
+    return Array.from(columns);
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-
-        const response = await axios.get(`/api/fetchProductSales?territory_id=${query.get("territory_id")}&date=${date}&partner=${partner}`)
+        setIsLoading(true);
+        const encodedTerritory = encodeURIComponent(territory_id);
+        const encodedDate = encodeURIComponent(date);
+        const encodedPartner = encodeURIComponent(partner);
+        const response = await axios.get(
+          `/api/fetchProductSales?territory_id=${encodedTerritory}&date=${encodedDate}&partner=${encodedPartner}`
+        );
 
         console.log(response.data);
+        setTableData(response.data);
+        setColumns(getUniqueColumns(response.data));
       } catch (err) {
         console.error("Error fetching data:", err);
+      }finally{
+        setIsLoading(false)
       }
     };
 
     fetchData();
-  }, [query.get("territory_id"), date, partner]);
+  }, [territory_id, date, partner]);
+
+  const handleEditClick = () => {
+    setIsEdit(!isEdit); // Toggle edit mode
+  };
 
   return (
     <div className="bg-white w-full px-4 py-4 rounded-xl shadow-md">
@@ -61,6 +89,7 @@ const LeaderBoardForm = () => {
           />
         ))}
       </div>
+
       {/* Calendar */}
       <div className="flex gap-2 my-4 items-center">
         <h1 className="font-semibold">Date Selector:</h1>
@@ -86,6 +115,52 @@ const LeaderBoardForm = () => {
           <li>Click Save button post entering the correct value below.</li>
         </ol>
       </div>
+
+      <div className="flex gap-2 justify-end items-center py-3 text-blue-600">
+        <button
+          className="border-2 border-gray-200 p-3 rounded-xl flex-row flex justify-center items-center gap-2 hover:bg-gray-300"
+          onClick={handleEditClick}
+        >
+          {isEdit ? "Save" : "Edit"} <FiEdit3 />
+        </button>
+      </div>
+
+      <table className="table-auto w-full my-3">
+        {/* Table Header */}
+        <thead>
+          <tr>
+            {columns.map((colName) => (
+              <th
+                key={colName}
+                className={`px-4 py-2 border border-gray-300 text-left`}
+              >
+                {colName}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        {/* Table Body */}
+        <tbody>
+          {tableData.map((row, rowIndex) => (
+            <tr
+              key={rowIndex}
+              className={`border border-gray-300`}
+            >
+              {columns.map((colName) => (
+                <td key={`${rowIndex}-${colName}`} className={` hover:bg-gray-100 ${colName === 'store_name' ? 'will-change-scroll w-96' : ''}`}>
+                  <input
+                    type="text"
+                    value={row[colName]}
+                    // onChange={(e) => handleChange(rowIndex, colName, e)}
+                    disabled={!isEdit}
+                    className="border border-gray-300 w-full px-4 py-1 outline-none focus:ring-1 focus:ring-blue-500 focus:border-transparent"
+                  />
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
