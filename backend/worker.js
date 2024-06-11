f5ff6b6dcd10d4b9bdc83abedbefe84718d5a32d
Joy Brar <joybrar2001@gmail.com>
2024-06-11 15:54:40 +0530

    fix

--- backend/worker.js
+++ backend/worker.js
@@ -64,10 +64,10 @@ wss.on('connection', (ws) => {
 // });
 // HTTP Server
 
-app.use(express.static(path.join(__dirname, '/../client/dist')));
+app.use(express.static(path.join(__dirname, '/../frontend/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
 });
 
 const server = app.listen(PORT, () => {
