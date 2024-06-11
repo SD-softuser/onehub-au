9566bad9d8b806e3e90f1766af33d7913d5e7ba8
Joy Brar <joybrar2001@gmail.com>
2024-06-12 00:13:04 +0530

    render hosting

--- backend/worker.js
+++ backend/worker.js
@@ -64,11 +64,11 @@ wss.on('connection', (ws) => {
 // });
 // HTTP Server
 
-// app.use(express.static(path.join(__dirname, '/../frontend/dist')));
+app.use(express.static(path.join(__dirname, '/../frontend/dist')));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
+});
 
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
