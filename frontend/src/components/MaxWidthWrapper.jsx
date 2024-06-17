87fc2d7b74ae6881fb112bc8d7b2bd3adda3de73
Joy Brar <joybrar2001@gmail.com>
2024-06-17 21:03:00 +0530

    MaxWidthWrapper

--- frontend/src/components/MaxWidthWrapper.jsx
+++ frontend/src/components/MaxWidthWrapper.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MaxWidthWrapper = ({ className, children }) => {
   return (
     // <div className='bg-[#F6F7FA] w-full h-full min-h-screen'>
-      <div className={`bg-[#F6F7FA] mx-auto w-full h-full px-2.5 md:px-8 font-googleSans ${className}`}>
+      <div className={`bg-[#F6F7FA] mx-auto w-full min-h-screen px-2.5 md:px-8 font-googleSans ${className}`}>
         {children}
       </div>
     // </div>
